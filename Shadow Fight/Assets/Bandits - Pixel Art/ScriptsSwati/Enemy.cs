76d5f19e2dd747e6c3243efef68d96d010b0471b
Swati01012002 <swati2111010@akgec.ac.in>
2023-06-14 18:01:19 +0530

    Hurt and Death animation added

--- Shadow Fight/Assets/Bandits - Pixel Art/ScriptsSwati/Enemy.cs
+++ Shadow Fight/Assets/Bandits - Pixel Art/ScriptsSwati/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+   public Animator animator;
    public int maxHealth = 100;
     int currentHealth;
 
@@ -16,6 +17,8 @@ public class Enemy : MonoBehaviour
     {
         currentHealth -= damage;
 
+        animator.SetTrigger("Hurt");
+
         if(currentHealth <= 0)
         {
             Die();
@@ -25,5 +28,10 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Debug.Log("Enemy died");
+
+        animator.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
     }
 }
