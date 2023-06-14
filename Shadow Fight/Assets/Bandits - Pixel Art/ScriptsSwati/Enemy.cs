f4e953a8e234f7ac798c9cbe80425dc7f8daa1ca
Swati01012002 <swati2111010@akgec.ac.in>
2023-06-14 23:19:52 +0530

    Background and Health Bar added

--- Shadow Fight/Assets/Bandits - Pixel Art/ScriptsSwati/Enemy.cs
+++ Shadow Fight/Assets/Bandits - Pixel Art/ScriptsSwati/Enemy.cs
@@ -7,15 +7,18 @@ public class Enemy : MonoBehaviour
    public Animator animator;
    public int maxHealth = 100;
     int currentHealth;
+   public HealthBar healthBar;
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
 
         animator.SetTrigger("Hurt");
 
